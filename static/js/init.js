848ac6250a62aadce3cc9b719f4b0db4fe5ff688
BryanDadiz <dabryan.devopstechnologiesph@gmail.com>
2024-04-04 11:41:21 +0800

    initial commit

--- static/js/init.js
+++ static/js/init.js
@@ -8,14 +8,3 @@
 
 $(".button-collapse").sideNav();
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems, options);
-  });
-
-  // Or with jQuery
-
-  $(document).ready(function(){
-    $('.modal').modal();
-  });
-       
