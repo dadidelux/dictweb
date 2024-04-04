bfb16d2420cc82e71abf550b18e87075f8c67cc0
BryanDadiz <dabryan.devopstechnologiesph@gmail.com>
2024-04-04 10:12:14 +0800

    initial commit

--- static/js/init.js
+++ static/js/init.js
@@ -5,3 +5,17 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+$(".button-collapse").sideNav();
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.modal').modal();
+  });
+       
